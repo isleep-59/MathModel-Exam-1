8f119aced7925c61dade0fd44a5b1b12976a5c3b
isleep-59 <goodwsj@qq.com>
2022-07-29 10:37:57 +0800

    添加项目文件。

---
+++ MathModel-Exam-1/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+}
+
+void init() {
+
+}
+
+signed main() {
+	freopen("in.txt", "r", stdin);
+
+	init();
+	solve();
+
+	return 0;
+}
