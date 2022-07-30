e2bf3021b7692c9ff172afe61d961ea48e956801
isleep-59 <goodwsj@qq.com>
2022-07-30 16:18:09 +0800

    upgrade "Adj_Graph_Node.h"

--- MathModel-Exam-1/Adj_Graph_Node.h
+++ MathModel-Exam-1/Adj_Graph_Node.h
@@ -1,15 +1,20 @@
 #pragma once
-#include "Train_Travel.h"
+#include "Train_Travel_Node.h"
 
 class Adj_Graph_Node {
 public:
-	Train_Travel info;
+	string leave_station, arrive_station;
+	int travel_index;
+	string train_name;
 
 	Adj_Graph_Node() {
 
 	}
 
-	Adj_Graph_Node(Train_Travel info) {
-		this->info = info;
+	Adj_Graph_Node(string leave_station = "", string arrive_station = "", int travel_index = 0, string train_name = "") {
+		this->leave_station = leave_station;
+		this->arrive_station = arrive_station;
+		this->travel_index = travel_index;
+		this->train_name = train_name;
 	}
 };
